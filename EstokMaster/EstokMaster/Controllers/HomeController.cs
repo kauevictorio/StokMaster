2046d35b98a2924648109b3da776f1491a1eb3a0
Kavik <78550884+kauevictorio@users.noreply.github.com>
2022-12-27 16:49:02 -0300

    First Commit StokMaster Study

--- EstokMaster/EstokMaster/Controllers/HomeController.cs
+++ EstokMaster/EstokMaster/Controllers/HomeController.cs
@@ -13,18 +13,13 @@ namespace EstokMaster.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Sobre()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
