7ae5d6758ef37a893be9f3c85b96c02992f7bf2e
Kaue <kaue.o.victorio@outlook.com>
2022-12-26 21:30:49 -0300

    StokMaster

---
+++ EstokMaster/EstokMaster/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EstokMaster.MvcApplication" Language="C#" %>
---
+++ EstokMaster/EstokMaster/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EstokMaster.Startup))]
+namespace EstokMaster
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
